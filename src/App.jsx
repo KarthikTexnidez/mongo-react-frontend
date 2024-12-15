52d621b4eb7d6f7aec7cc3e48e01d4a6f9c871df
Karthik Texnidez <texnidez3d@gmail.com>
2024-12-15 13:19:11 +0530

    commitinitial

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sentData = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/entry", { name, email, password })
+      .then((result) => console.log(result), alert("Data Send"))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor="name">Full Name</label> <br />
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />{" "}
+        <br />
+        <label htmlFor="email">Email</label> <br />
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />{" "}
+        <br />
+        <label htmlFor="password">Password</label> <br />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+        <br /> <br />
+        <button onClick={sentData}>Send Data</button>
+      </form>
+    </>
+  );
+};
+
+export default App;
